e86bd6e103cb33abfbe4f9a850890dfc19bf5931
Mdots <mdotstrange@gmail.com>
2020-03-18 19:46:28 -0700

    Added BD Sender script for second tutorial

---
+++ Assets/Scripts/BehaviorDesignerEventSender.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner;
+using BehaviorDesigner.Runtime;
+
+public class BehaviorDesignerEventSender : MonoBehaviour
+{
+    public BehaviorTree BehaviorTree;
+
+
+    public void SendBtEvent(string theEventToFire)
+    {
+        BehaviorTree.SendEvent(theEventToFire);
+    }
+	
+}
