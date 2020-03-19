ce9cae3cb55106b4bcfc6d0b56c4793e5be77d20
Mdots <mdotstrange@gmail.com>
2020-03-18 18:51:51 -0700

    Setting tutorial directory
    Only scripts are updated

---
+++ Assets/Scripts/NavmeshAgentSetsAnimatorParameters.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavmeshAgentSetsAnimatorParameters : MonoBehaviour
+{
+    public Animator Animator;
+    public string BoolVariableName;
+    public string FloatVariableName;
+    public Vector2 FromFloatMinMax;
+    public Vector2 ToFloatMinMax;
+    public NavMeshAgent NavMeshAgent;
+
+
+    private void Update()
+    {
+        if(BoolVariableName != "")
+        {
+            UpdateAnimatorBool();
+        }
+
+        if(FloatVariableName != "")
+        {
+            UpdateAnimatorFloat();
+        }
+               
+    }
+
+
+    void UpdateAnimatorBool()
+    {
+       if(NavMeshAgent.velocity.magnitude > 0 )
+        {
+            Animator.SetBool(BoolVariableName, true);
+        }
+       else
+        {
+            Animator.SetBool(BoolVariableName, false);
+        }
+    }
+
+    void UpdateAnimatorFloat()
+    {
+        float adjustedVelocity = Remap(NavMeshAgent.velocity.magnitude, FromFloatMinMax.x, FromFloatMinMax.y, ToFloatMinMax.x, ToFloatMinMax.y);
+
+        Animator.SetFloat(FloatVariableName, adjustedVelocity);
+    }
+
+    public float Remap(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
+
+
+}
